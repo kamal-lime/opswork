9682e28e2a256ad349179ed62b53713485c8803a
kamal-lime <kamalk@limeroad.com>
2017-01-30 13:24:27 +0530

    Update apache.rb

--- apache.rb
+++ apache.rb
@@ -1,3 +1,28 @@
-package "apache2" do
-  action :install
+script "apache" do
+  interpreter "bash"
+  user "ubuntu"
+  group "ubuntu"
+  
+
+  code1 = '';
+  code1 +=  'sudo mount /dev/xvdf /mnt/solr;
+  sudo rm /var/log/jetty/*;
+  sudo rm /var/cache/jetty/tmp/*;
+  sudo rm /mnt/solr/log/jetty/*;
+  USED=$(df /dev/xvdf | awk \'{print $5} \'| sed -ne 2p | cut -d"%" -f1)
+  if [ $USED -gt 30 ]
+  then
+  sudo rm -rf /mnt/solr1
+
+  fi
+  sudo service apache2 restart;
+  
+
+  puts code1
+  result = system(code1)
+  if !result.present?
+      fail "Deployment failed!!!"
+  end
+  sleep 300
+
 end
